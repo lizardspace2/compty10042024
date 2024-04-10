a003bbe10cc73ab0d10a694685b29f5200982c22
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-04-10 20:47:09 +0000

    zzzzzzzzzzzzzzzzzzzza

--- src/components/Pilotage/components/MyChartComponent.jsx
+++ src/components/Pilotage/components/MyChartComponent.jsx
@@ -13,6 +13,7 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  Rectangle,
 } from 'recharts';
 
 const data = [
@@ -30,27 +31,32 @@ const data = [
     { name: 'déc.', Revenues: 1890, Expenses: 4800, Result: -2910 }
   ];  
 
+const CustomBar = (props) => {
+  const { fill, x, y, width, height } = props;
+
+  return <Rectangle radius={[10, 10, 0, 0]} fill={fill} x={x} y={y} width={width} height={height} />;
+};
 
 // Custom tooltip to match the design
 const CustomTooltip = ({ active, payload, label }) => {
-  if (active && payload && payload.length) {
-    return (
-      <Box bg="gray.700" color="white" p="2" borderRadius="md" boxShadow="xl">
-        <p>{`${label}`}</p>
-        <p>{`Revenues: ${payload[0].value}€`}</p>
-        <p>{`Expenses: ${payload[1].value}€`}</p>
-        <p>{`Result: ${payload[2].value}€`}</p>
-      </Box>
-    );
-  }
-
-  return null;
-};
+    if (active && payload && payload.length) {
+      return (
+        <Box bg="gray.700" color="white" p="2" borderRadius="md" boxShadow="xl">
+          <p>{`${label}`}</p>
+          <p>{`Revenues: ${payload[0].value}€`}</p>
+          <p>{`Expenses: ${payload[1].value}€`}</p>
+          <p>{`Result: ${payload[2].value}€`}</p>
+        </Box>
+      );
+    }
+  
+    return null;
+  };
 
 function MyChartComponent() {
   const bgColor = useColorModeValue('white', 'gray.800'); // Conditional background color
   const borderColor = useColorModeValue('gray.200', 'gray.700'); // Conditional border color
-  
+
   return (
     <Box
       p={4}
@@ -68,9 +74,16 @@ function MyChartComponent() {
           <YAxis yAxisId="right" orientation="right" stroke="#82ca9d" />
           <Tooltip content={<CustomTooltip />} />
           <Legend />
-          <Bar yAxisId="left" dataKey="Expenses" barSize={20} fill="#8884d8" />
-          <Bar yAxisId="left" dataKey="Revenues" barSize={20} fill="#82ca9d" />
-          <Line yAxisId="right" type="monotone" dataKey="Result" stroke="#ff7300" />
+          <Bar yAxisId="left" dataKey="Expenses" barSize={20} fill="#EB6B9D" shape={<CustomBar />} />
+          <Bar yAxisId="left" dataKey="Revenues" barSize={20} fill="#7DD3FC" shape={<CustomBar />} />
+          <Line 
+            yAxisId="right" 
+            type="monotone" 
+            dataKey="Result" 
+            stroke="#333333" 
+            strokeWidth={2}
+            dot={{ fill: '#333333', stroke: '#fff', strokeWidth: 2, r: 5 }}
+          />
         </ComposedChart>
       </ResponsiveContainer>
     </Box>
