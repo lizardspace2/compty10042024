a6c24620af8fec87640d5ff379dd6bbf1d7d0def
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-04-10 19:01:00 +0000

    sdsqdsqsqsss

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,31 +10,31 @@ function Navbar() {
           <Text fontSize="lg" fontWeight="bold">indy</Text>
         </Box>
         <Divider />
-        <Button leftIcon={<Icon as={FaUserCircle} />} justifyContent="flex-start" variant="ghost">
+        <Button leftIcon={<Icon as={FaUserCircle} />} justifyContent="flex-start" variant="ghost" rounded="full">
           Bonjour Guillaume
         </Button>
-        <Button leftIcon={<Icon as={FaHome} />} justifyContent="flex-start" variant="ghost">
+        <Button leftIcon={<Icon as={FaHome} />} justifyContent="flex-start" variant="ghost" rounded="full">
           Pilotage
         </Button>
-        <Button leftIcon={<Icon as={FaRegCreditCard} />} justifyContent="flex-start" variant="ghost">
+        <Button leftIcon={<Icon as={FaRegCreditCard} />} justifyContent="flex-start" variant="ghost" rounded="full">
           Transactions
         </Button>
-        <Button leftIcon={<Icon as={FaRegListAlt} />} justifyContent="flex-start" variant="ghost" position="relative">
+        <Button leftIcon={<Icon as={FaRegListAlt} />} justifyContent="flex-start" variant="ghost" position="relative" rounded="full">
           À faire
           <Text as="span" position="absolute" top="-1" right="-1" p="1" fontSize="xs" fontWeight="bold" color="white" bg="red.500" borderRadius="full">
             1
           </Text>
         </Button>
-        <Button leftIcon={<Icon as={FaRegFileAlt} />} justifyContent="flex-start" variant="ghost">
+        <Button leftIcon={<Icon as={FaRegFileAlt} />} justifyContent="flex-start" variant="ghost" rounded="full">
           Documents
         </Button>
-        <Button leftIcon={<Icon as={FaUserFriends} />} justifyContent="flex-start" variant="ghost">
+        <Button leftIcon={<Icon as={FaUserFriends} />} justifyContent="flex-start" variant="ghost" rounded="full">
           Accompagnement
         </Button>
         <Divider />
       </VStack>
       {/* Separated Parrainage button */}
-      <Button leftIcon={<Icon as={FaPaperPlane} />} justifyContent="flex-start" variant="ghost" mt="auto">
+      <Button leftIcon={<Icon as={FaPaperPlane} />} justifyContent="flex-start" variant="ghost" mt="auto" rounded="full">
         Parrainage
       </Button>
     </Box>
