e559ea09c4c5cfcf62b0956ebb7b94a48a9a1c44
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-04-10 19:48:46 +0000

    sssssssssss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
+// App.js
 import React from 'react';
-import { ChakraProvider, Box, Flex, Text } from '@chakra-ui/react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import necessary components from react-router-dom
-import Navbar from './components/Navbar'; 
-
-// Import your other components/pages
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Pilotage from './components/Pilotage';
 import Transactions from './components/Transactions';
@@ -14,26 +12,20 @@ import Sponsorship from './components/Sponsorship';
 
 function App() {
   return (
-    <ChakraProvider>
-      <Flex h="100vh">
-        <Box minW="200px">
-          <Navbar />
-        </Box>
-        <Box flex="1" p={5}>
-          <Router>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/pilotage" element={<Pilotage />} />
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pilotage" element={<Pilotage />} />
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/todo" element={<Todo />} />
               <Route path="/documents" element={<Documents />} />
               <Route path="/accompaniment" element={<Accompaniment />} />
               <Route path="/sponsorship" element={<Sponsorship />} />
-            </Routes>
-          </Router>
-        </Box>
-      </Flex>
-    </ChakraProvider>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
