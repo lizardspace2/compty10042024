8811e575f847fc03c06f5911de06a2c0e6c54b65
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-04-10 19:54:33 +0000

    ddddddddddddddd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, VStack, Text, Divider, Icon } from '@chakra-ui/react';
+import { Box, VStack, Text, Divider, Icon, Button } from '@chakra-ui/react';
 import { FaHome, FaRegListAlt, FaRegCreditCard, FaRegFileAlt, FaUserFriends, FaUserCircle, FaPaperPlane } from 'react-icons/fa';
 import { Link } from 'react-router-dom'; // Import Link component from React Router
 
@@ -12,52 +12,52 @@ function Navbar() {
         </Box>
         <Divider />
         <Link to="/profile" style={{ textDecoration: 'none' }}>
-          <Box as="button" display="flex" justifyContent="flex-start" variant="ghost" rounded="full">
+          <Button as="button" display="flex" justifyContent="flex-start" variant="ghost" rounded="full">
             <Icon as={FaUserCircle} />
             <Text ml={2}>Bonjour Guillaume</Text>
-          </Box>
+          </Button>
         </Link>
         <Link to="/dashboard" style={{ textDecoration: 'none' }}>
-          <Box as="button" display="flex" justifyContent="flex-start" variant="ghost" rounded="full">
+          <Button as="button" display="flex" justifyContent="flex-start" variant="ghost" rounded="full">
             <Icon as={FaHome} />
             <Text ml={2}>Pilotage</Text>
-          </Box>
+          </Button>
         </Link>
         <Link to="/transactions" style={{ textDecoration: 'none' }}>
-          <Box as="button" display="flex" justifyContent="flex-start" variant="ghost" rounded="full">
+          <Button as="button" display="flex" justifyContent="flex-start" variant="ghost" rounded="full">
             <Icon as={FaRegCreditCard} />
             <Text ml={2}>Transactions</Text>
-          </Box>
+          </Button>
         </Link>
         <Link to="/tasks" style={{ textDecoration: 'none' }}>
-          <Box as="button" display="flex" justifyContent="flex-start" variant="ghost" rounded="full" position="relative">
+          <Button as="button" display="flex" justifyContent="flex-start" variant="ghost" rounded="full" position="relative">
             <Icon as={FaRegListAlt} />
             <Text ml={2}>À faire</Text>
             <Text as="span" position="absolute" top="-1" right="-1" p="1" fontSize="xs" fontWeight="bold" color="white" bg="red.500" borderRadius="full">
               1
             </Text>
-          </Box>
+          </Button>
         </Link>
         <Link to="/documents" style={{ textDecoration: 'none' }}>
-          <Box as="button" display="flex" justifyContent="flex-start" variant="ghost" rounded="full">
+          <Button as="button" display="flex" justifyContent="flex-start" variant="ghost" rounded="full">
             <Icon as={FaRegFileAlt} />
             <Text ml={2}>Documents</Text>
-          </Box>
+          </Button>
         </Link>
         <Link to="/accompaniment" style={{ textDecoration: 'none' }}>
-          <Box as="button" display="flex" justifyContent="flex-start" variant="ghost" rounded="full">
+          <Button as="button" display="flex" justifyContent="flex-start" variant="ghost" rounded="full">
             <Icon as={FaUserFriends} />
             <Text ml={2}>Accompagnement</Text>
-          </Box>
+          </Button>
         </Link>
         <Divider />
       </VStack>
       {/* Separated Parrainage button */}
       <Link to="/sponsorship" style={{ textDecoration: 'none' }}>
-        <Box as="button" display="flex" justifyContent="flex-start" variant="ghost" mt="auto" rounded="full">
+        <Button as="button" display="flex" justifyContent="flex-start" variant="ghost" mt="auto" rounded="full">
           <Icon as={FaPaperPlane} />
           <Text ml={2}>Parrainage</Text>
-        </Box>
+        </Button>
       </Link>
     </Box>
   );
