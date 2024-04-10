5584bb3b337becb9025d4fb02273f80da99ae6ad
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-04-10 19:50:26 +0000

    sssssssssssssss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ChakraProvider, Box, Flex, Text } from '@chakra-ui/react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Pilotage from './components/Pilotage';
@@ -12,20 +12,26 @@ import Sponsorship from './components/Sponsorship';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pilotage" element={<Pilotage />} />
+    <ChakraProvider>
+      <Router>
+        <Flex h="100vh">
+          <Box minW="200px">
+            <Navbar />
+          </Box>
+          <Box flex="1" p={5}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/pilotage" element={<Pilotage />} />
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/todo" element={<Todo />} />
               <Route path="/documents" element={<Documents />} />
               <Route path="/accompaniment" element={<Accompaniment />} />
               <Route path="/sponsorship" element={<Sponsorship />} />
-        </Routes>
-      </div>
-    </Router>
+            </Routes>
+          </Box>
+        </Flex>
+      </Router>
+    </ChakraProvider>
   );
 }
 
