a1c0aaa6e494fd1497f133e5198f9183db257c21
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-04-12 15:02:00 +0000

    erererererererererererererererer

--- src/components/Pilotage/Afaire/components/UpcomingTasks.jsx
+++ src/components/Pilotage/Afaire/components/UpcomingTasks.jsx
@@ -10,7 +10,6 @@ import {
   Button,
   VStack,
   Divider,
-  IconButton,
   CloseButton,
 } from '@chakra-ui/react';
 import { FcOvertime } from 'react-icons/fc';
@@ -23,10 +22,14 @@ const TaskItem = ({ title, dateRange, exerciseYear, status, time, buttonText }) 
       borderRadius="lg"
       align="center"
       justify="space-between"
-      // Make sure the container fits the content
       w="full"
+      borderLeftWidth="10px" // Add a left border with width of 10px
+      borderColor="gray.200" // Set the border color (adjust as needed)
+      _first={{
+        borderColor: "blue.500", // First item border color
+      }}
     >
-      <VStack align="start" spacing={1} flex="1">  // flex="1" will allow this stack to expand
+      <VStack align="start" spacing={1} flex="1">
         <Flex align="center">
           <Box as={FcOvertime} mr={2} />
           <Text fontSize="sm" color="gray.500">{dateRange}</Text>
@@ -37,7 +40,7 @@ const TaskItem = ({ title, dateRange, exerciseYear, status, time, buttonText }) 
           <Badge ml={2} colorScheme="red" borderRadius="full">{status}</Badge>
         </Flex>
       </VStack>
-      <VStack align="end">  // VStack for vertical alignment of time and button
+      <VStack align="end">
         <Box as={FcOvertime} size="20px" />
         <Text color="gray.500">{time}</Text>
         <Button colorScheme="teal" size="sm">{buttonText}</Button>
@@ -49,13 +52,12 @@ const TaskItem = ({ title, dateRange, exerciseYear, status, time, buttonText }) 
 const UpcomingTasks = () => {
   return (
     <Box width="100%" maxW="400px" p={4} mr={4} position="relative">
-      <CloseButton position="absolute" right="4" top="4" /> 
+      <CloseButton position="absolute" right="4" top="4" />
       <Heading size="md" mb={4}>
         À venir
       </Heading>
       <Divider mb={4} />
       <Stack spacing={4}>
-        {/* You can map over your tasks here */}
         <TaskItem
           title="Déclaration sociale et fiscale"
           dateRange="18 avr. 2024 - 31 mai 2024"
