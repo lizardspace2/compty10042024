b61a87b4f7db74a70e7d8d047017fbbec6a1cf26
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-04-12 21:26:12 +0000

    dsvfsdfssddfsds

--- src/components/Accompagnement/Accompaniment.jsx
+++ src/components/Accompagnement/Accompaniment.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from '@chakra-ui/react';
+import { Container, Grid, VStack } from '@chakra-ui/react';
 import SpecialistsComponent from './components/SpecialistsComponent';
 import TitleSubtitleComponent from './components/TitleSubtitleComponent';
 import KnowledgeBaseCard from './components/KnowledgeBaseCard';
@@ -10,23 +10,26 @@ import CreerUneSociete from './components/CreerUneSociete';
 
 function Accompaniment() {
   return (
-    <div>
-      <h1>Accompaniment Page</h1>
-      <SpecialistsComponent/>
-      <TitleSubtitleComponent/>
+    <Container maxW="container.xl" centerContent>
+      <VStack spacing={8} align="stretch">
+        <h1>Accompaniment Page</h1>
+        <SpecialistsComponent />
+        <TitleSubtitleComponent />
 
-      {/* Responsive Grid for Knowledge Base and Help Center Cards */}
-      <Grid templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)' }} gap={6}>
-        <KnowledgeBaseCard/>
-        <HelpCenterCard/>
-      </Grid>
+        {/* Responsive Grid for Knowledge Base and Help Center Cards */}
+        <Grid templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)' }} gap={6}>
+          <KnowledgeBaseCard />
+          <HelpCenterCard />
+        </Grid>
 
-      <AllerPlusLoin/>
-      <Grid templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)' }} gap={6} my={6}>
-        <CreerUneSci/>
-        <CreerUneSociete/>
-      </Grid>
-    </div>
+        <AllerPlusLoin />
+        
+        <Grid templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)' }} gap={6}>
+          <CreerUneSci />
+          <CreerUneSociete />
+        </Grid>
+      </VStack>
+    </Container>
   );
 }
 
