f79c3a060ff2fef5292ae19a3620f9b7221f0de3
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-04-13 20:04:19 +0000

    dssfdfsfdf

--- src/components/Transactions/components/AccountSummary.jsx
+++ src/components/Transactions/components/AccountSummary.jsx
@@ -3,7 +3,6 @@ import {
   Box,
   Flex,
   Text,
-  VStack,
   useDisclosure,
   Button,
   Menu,
@@ -17,25 +16,33 @@ const AccountSummary = () => {
   const { isOpen, onToggle } = useDisclosure();
 
   return (
-    <Box p={4} borderRadius="lg" borderWidth="1px" borderColor="gray.200" w="full">
+    <Box p={4} bg="white" maxW="500px" w="full" mx="auto">
       <Menu isOpen={isOpen}>
-        <MenuButton as={Button} rightIcon={isOpen ? <FaChevronUp /> : <FaChevronDown />} onClick={onToggle} w="full" justifyContent="space-between">
-          <Box>
+        <MenuButton as={Button} rightIcon={isOpen ? <FaChevronUp /> : <FaChevronDown />} onClick={onToggle} maxW="300px" minW="300px" >
+          <Flex justifyContent="space-between" alignItems="center">
             <Text fontWeight="semibold">Solde total:</Text>
             <Text fontWeight="bold">4 972,79 €</Text>
-          </Box>
+          </Flex>
         </MenuButton>
         <MenuList>
-          <VStack alignItems="flex-start" p={4}>
-            <MenuItem>
-              <Flex justifyContent="space-between" w="full">
-                <Text fontWeight="bold">1. Boursobank (ex Boursorama)</Text>
-                <Text fontWeight="bold">4 972,79 €</Text>
+          <MenuItem minH="48px">
+            <Box w="full">
+              <Flex justify="space-between" align="center" w="full">
+                <Box>
+                  <Text fontWeight="bold" noOfLines={1}>
+                    1. Boursobank (ex Boursorama)
+                  </Text>
+                  <Text fontSize="sm">M Recipon Guillaume</Text>
+                </Box>
+                <Box textAlign="right">
+                  <Text fontWeight="bold" noOfLines={1}>
+                    4 972,79 €
+                  </Text>
+                  <Text fontSize="sm">le 13/04/2024</Text>
+                </Box>
               </Flex>
-            </MenuItem>
-            <Text fontSize="sm" alignSelf="flex-end">M Recipon Guillaume</Text>
-            <Text fontSize="sm" alignSelf="flex-end">le 13/04/2024</Text>
-          </VStack>
+            </Box>
+          </MenuItem>
         </MenuList>
       </Menu>
     </Box>
