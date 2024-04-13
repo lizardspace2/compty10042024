7d9cd9250750221e26e47c13e4cdd8e85186a7e6
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-04-13 14:58:55 +0000

    fdsfdsdfds

--- src/components/Pilotage/components/surlecote/RevenueComponent.jsx
+++ src/components/Pilotage/components/surlecote/RevenueComponent.jsx
@@ -4,12 +4,25 @@ import { IoIosCash } from 'react-icons/io';
 
 const RevenueComponent = ({ amount, currency }) => {
   return (
-    <Box p={4} boxShadow="md" borderRadius="md" display="flex" alignItems="center">
-      <IoIosCash size="2em" color="#abe3fd" />
-      <Stat ml={2}>
-        <StatLabel color="gray.500">Revenues</StatLabel>
-        <StatNumber fontSize="2xl">
-          20 000 €
+    <Box
+      p={2} // smaller padding
+      boxShadow="md"
+      borderRadius="md"
+      display="flex"
+      alignItems="center"
+      border="1px" // 1px border
+      borderColor="gray.200" // light gray border color
+      width="auto" // width adjusted to content
+      bg="white" // white background
+      maxW="200px"
+    >
+      <Box color="#abe3fd" mr={2}>
+        <IoIosCash size="1.5em" /> {/* Adjust icon size as needed */}
+      </Box>
+      <Stat>
+        <StatLabel color="gray.500" fontSize="sm">Revenues</StatLabel>
+        <StatNumber fontSize="lg" lineHeight="1.1em">
+          127 €
         </StatNumber>
       </Stat>
     </Box>
