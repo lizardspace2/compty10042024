20e7806c97a0d58611b81550bcadeb9f5e1ae927
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-04-13 19:50:22 +0000

    fhgffdgdfgfd

--- src/components/Transactions/components/AccountSummary.jsx
+++ src/components/Transactions/components/AccountSummary.jsx
@@ -1,56 +1,44 @@
 import React from 'react';
-import { Box, Text, Flex, VStack, HStack, Icon, useColorModeValue } from '@chakra-ui/react';
-import { FaChevronDown } from 'react-icons/fa';
+import {
+  Box,
+  Flex,
+  Text,
+  VStack,
+  useDisclosure,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from '@chakra-ui/react';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 
 const AccountSummary = () => {
-  const bgColor = useColorModeValue('white', 'gray.700');
-  const textColor = useColorModeValue('gray.700', 'white');
-  const subTextColor = useColorModeValue('gray.500', 'gray.200');
+  const { isOpen, onToggle } = useDisclosure();
 
   return (
-    <VStack
-      bg={bgColor}
-      p={4}
-      borderRadius="lg"
-      borderWidth="1px"
-      borderColor={useColorModeValue('gray.200', 'gray.600')}
-      w="full"
-      alignItems="flex-start"
-    >
-      <HStack justifyContent="space-between" w="full">
-        <Text fontWeight="semibold" fontSize="lg" color={textColor}>
-          Solde total:
-        </Text>
-        <Box>
-          <Flex alignItems="center" color={textColor}>
-            <Text fontWeight="bold" fontSize="xl">
-              4 972,79 €
-            </Text>
-            <Icon as={FaChevronDown} ml={1} />
-          </Flex>
-        </Box>
-      </HStack>
-      <Box pt={4} w="full">
-        <Flex justifyContent="space-between" w="full">
-          <VStack alignItems="flex-start">
-            <Text fontWeight="bold" color={textColor}>
-              1. Boursobank (ex Boursorama)
-            </Text>
-            <Text fontSize="sm" color={subTextColor}>
-              M Recipon Guillaume
-            </Text>
+    <Box p={4} borderRadius="lg" borderWidth="1px" borderColor="gray.200" w="full">
+      <Menu isOpen={isOpen}>
+        <MenuButton as={Button} rightIcon={isOpen ? <FaChevronUp /> : <FaChevronDown />} onClick={onToggle} w="full" justifyContent="space-between">
+          <Box>
+            <Text fontWeight="semibold">Solde total:</Text>
+            <Text fontWeight="bold">4 972,79 €</Text>
+          </Box>
+        </MenuButton>
+        <MenuList>
+          <VStack alignItems="flex-start" p={4}>
+            <MenuItem>
+              <Flex justifyContent="space-between" w="full">
+                <Text fontWeight="bold">1. Boursobank (ex Boursorama)</Text>
+                <Text fontWeight="bold">4 972,79 €</Text>
+              </Flex>
+            </MenuItem>
+            <Text fontSize="sm" alignSelf="flex-end">M Recipon Guillaume</Text>
+            <Text fontSize="sm" alignSelf="flex-end">le 13/04/2024</Text>
           </VStack>
-          <VStack alignItems="flex-end">
-            <Text fontWeight="bold" fontSize="lg" color={textColor}>
-              4 972,79 €
-            </Text>
-            <Text fontSize="sm" color={subTextColor}>
-              le 13/04/2024
-            </Text>
-          </VStack>
-        </Flex>
-      </Box>
-    </VStack>
+        </MenuList>
+      </Menu>
+    </Box>
   );
 };
 
