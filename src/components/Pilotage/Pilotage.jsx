36561abca86c8a2abdf4778208ef04c454917b0f
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-04-13 15:07:44 +0000

    sdreree

--- src/components/Pilotage/Pilotage.jsx
+++ src/components/Pilotage/Pilotage.jsx
@@ -1,5 +1,5 @@
-// Pilotage.js
 import React from 'react';
+import { Box, Grid, GridItem } from '@chakra-ui/react';
 import MyChartComponent from './components/MyChartComponent';
 import RevenueTable from './components/RevenueTable';
 import DepenseTable from './components/DepenseTable';
@@ -7,7 +7,6 @@ import ThresholdProgressBar from './components/ThresholdProgressBar';
 import ExpensesBarChart from './components/ExpensesBarChart';
 import TreasuryChart from './components/TreasuryChart';
 import PilotageBanner from './components/PilotageBanner';
-import { Box } from '@chakra-ui/react';
 import RevenueComponent from './components/surlecote/RevenueComponent';
 import ExpensesComponent from './components/surlecote/ExpensesComponent';
 import ResultComponent from './components/surlecote/ResultComponent';
@@ -15,17 +14,23 @@ import ResultComponent from './components/surlecote/ResultComponent';
 function Pilotage() {
   return (
     <div>
-      <PilotageBanner/>
-      <Box maxWidth="1400px" textAlign="center" mx="auto" >
-        <RevenueComponent/>
-        <ExpensesComponent/>
-        <ResultComponent/>
-      <MyChartComponent/>
-      <RevenueTable/>
-      <DepenseTable/>
-      <ThresholdProgressBar/>
-      <ExpensesBarChart/>
-      <TreasuryChart/>
+      <PilotageBanner />
+      <Box maxWidth="1400px" mx="auto" overflow="hidden">
+        <Grid templateColumns={{ sm: '1fr', md: '1fr 3fr' }} gap={6} textAlign="center">
+          <GridItem>
+            <Box mb={4}><RevenueComponent amount={127000} currency="€" /></Box>
+            <Box mb={4}><ExpensesComponent amount={85000} currency="€" /></Box>
+            <Box><ResultComponent amount={42000} currency="€" /></Box>
+          </GridItem>
+          <GridItem>
+            <MyChartComponent />
+          </GridItem>
+        </Grid>
+        <RevenueTable />
+        <DepenseTable />
+        <ThresholdProgressBar />
+        <ExpensesBarChart />
+        <TreasuryChart />
       </Box>
     </div>
   );
