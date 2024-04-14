0df38b44a2d79251d873e960cd5820ded5e2b030
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-04-14 21:03:25 +0000

    dfsssssssssss

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Todo from './components/Afaire/Todo';
 import Documents from './components/Documents/Documents';
 import Accompaniment from './components/Accompagnement/Accompaniment';
 import Sponsorship from './components/Sponsorship';
-import D2035 from './components/D2035/D2035'; // Make sure the path is correct
+import D2035 from './components/D2035/D2035'; // Assurez-vous que le chemin est correct
 
 function App() {
   return (
@@ -17,6 +17,26 @@ function App() {
       <Router>
         <Routes>
           <Route path="/d2035" element={<D2035 />} />
+          <Route path="/catégoriser-toutes-les-transactions" element={<CatégoriserToutesLesTransactions />} />
+          <Route path="/immobilisations-amortissements-et-cessions" element={<ImmobilisationsAmortissementsEtCessions />} />
+          <Route path="/ventiler-les-repas-hors-domicile" element={<VentilerLesRepasHorsDomicile />} />
+          <Route path="/retraiter-les-frais-de-véhicule" element={<RetraiterLesFraisDeVéhicule />} />
+          <Route path="/ventiler-les-échéances-d-emprunts" element={<VentilerLesÉchéancesDEmprunts />} />
+          <Route path="/chèques-non-encaissés-de-2022" element={<ChèquesNonEncaissésDe2022 />} />
+          <Route path="/chèques-non-encaissés-de-2023" element={<ChèquesNonEncaissésDe2023 />} />
+          <Route path="/saisir-les-frais-de-blanchissage-à-domicile" element={<SaisirLesFraisDeBlanchissageÀDomicile />} />
+          <Route path="/barème-kilométrique-forfaitaire" element={<BarèmeKilométriqueForfaitaire />} />
+          <Route path="/vérifier-les-comptes-financiers" element={<VérifierLesComptesFinanciers />} />
+          <Route path="/évolution-de-la-caisse" element={<ÉvolutionDeLaCaisse />} />
+          <Route path="/cotisations-urssaf-à-ventiler" element={<CotisationsUrssafÀVentiler />} />
+          <Route path="/saisie-du-compte-commun-ou-de-la-scm" element={<SaisieDuCompteCommunOuDeLaSCM />} />
+          <Route path="/réductions-d-impôt-pour-les-frais-de-comptabilité" element={<RéductionsDImpôtPourLesFraisDeComptabilité />} />
+          <Route path="/cotisations-facultatives-déductibles" element={<CotisationsFacultativesDéductibles />} />
+          <Route path="/exonérations-fiscales-et-crédits-d-impôts" element={<ExonérationsFiscalesEtCréditsDImpôts />} />
+          <Route path="/identité-de-l-entrepreneur" element={<IdentitéDeLEntrepreneur />} />
+          <Route path="/informations-relatives-à-l-activité" element={<InformationsRelativesÀLActivité />} />
+          <Route path="/informations-relatives-à-l-exercice-fiscal" element={<InformationsRelativesÀLExerciceFiscal />} />
+          <Route path="/valider-la-clôture" element={<ValiderLaClôture />} />
           <Route path="*" element={<LayoutWithSidebar />} />
         </Routes>
       </Router>
@@ -49,6 +69,87 @@ const LayoutWithSidebar = () => {
       </Box>
     </Flex>
   );
+};
+
+// Composants pour les nouvelles routes
+const CatégoriserToutesLesTransactions = () => {
+  return <div>Catégoriser toutes les transactions</div>;
+};
+
+const ImmobilisationsAmortissementsEtCessions = () => {
+  return <div>Immobilisations, amortissements et cessions</div>;
+};
+
+const VentilerLesRepasHorsDomicile = () => {
+  return <div>Ventiler les repas hors domicile</div>;
+};
+
+const RetraiterLesFraisDeVéhicule = () => {
+  return <div>Retraiter les frais de véhicule</div>;
+};
+
+const VentilerLesÉchéancesDEmprunts = () => {
+  return <div>Ventiler les échéances d'emprunts</div>;
+};
+
+const ChèquesNonEncaissésDe2022 = () => {
+  return <div>Chèques non encaissés de 2022</div>;
+};
+
+const ChèquesNonEncaissésDe2023 = () => {
+  return <div>Chèques non encaissés de 2023</div>;
+};
+
+const SaisirLesFraisDeBlanchissageÀDomicile = () => {
+  return <div>Saisir les frais de blanchissage à domicile</div>;
+};
+
+const BarèmeKilométriqueForfaitaire = () => {
+  return <div>Barème kilométrique forfaitaire</div>;
+};
+
+const VérifierLesComptesFinanciers = () => {
+  return <div>Vérifier les comptes financiers</div>;
+};
+
+const ÉvolutionDeLaCaisse = () => {
+  return <div>Évolution de la caisse</div>;
+};
+
+const CotisationsUrssafÀVentiler = () => {
+  return <div>Cotisations Urssaf à ventiler</div>;
+};
+
+const SaisieDuCompteCommunOuDeLaSCM = () => {
+  return <div>Saisie du compte commun ou de la SCM</div>;
+};
+
+const RéductionsDImpôtPourLesFraisDeComptabilité = () => {
+  return <div>Réductions d'impôt pour les frais de comptabilité</div>;
+};
+
+const CotisationsFacultativesDéductibles = () => {
+  return <div>Cotisations facultatives déductibles</div>;
+};
+
+const ExonérationsFiscalesEtCréditsDImpôts = () => {
+  return <div>Exonérations fiscales et crédits d'impôts</div>;
+};
+
+const IdentitéDeLEntrepreneur = () => {
+  return <div>Identité de l'entrepreneur</div>;
+};
+
+const InformationsRelativesÀLActivité = () => {
+  return <div>Informations relatives à l'activité</div>;
+};
+
+const InformationsRelativesÀLExerciceFiscal = () => {
+  return <div>Informations relatives à l'exercice fiscal</div>;
+};
+
+const ValiderLaClôture = () => {
+  return <div>Valider la clôture</div>;
 };
 
 export default App;
