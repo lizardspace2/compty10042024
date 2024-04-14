6fc5413be14e1bac17ecb77bd0e01b0ff9a7a301
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-04-14 04:15:47 +0000

    ggglj

--- src/components/Transactions/components/transactiondetail/VentilationComponent.jsx
+++ src/components/Transactions/components/transactiondetail/VentilationComponent.jsx
@@ -10,13 +10,13 @@ import {
   Stack,
   IconButton,
   InputGroup,
-  InputLeftElement,
+  InputRightElement,
   Text,
   useColorModeValue,
   Tooltip,
 } from '@chakra-ui/react';
-import { FaPlus, FaPercent } from 'react-icons/fa'; // Import FaPercent for percentage
-import { MdEuro } from 'react-icons/md'; // Import MdEuro for euro symbol
+import { FaPlus, FaPercent } from 'react-icons/fa';
+import { MdEuro } from 'react-icons/md';
 import { FcFullTrash } from 'react-icons/fc';
 
 const VentilationComponent = () => {
@@ -24,6 +24,7 @@ const VentilationComponent = () => {
     { category: 'Dépense personnelle', amount: '-0.99', percentage: 100 },
   ]);
 
+  const iconColor = useColorModeValue('gray.200', 'gray.300');
   const bgColor = useColorModeValue('gray.50', 'gray.700');
   const borderColor = useColorModeValue('gray.200', 'gray.600');
 
@@ -43,52 +44,48 @@ const VentilationComponent = () => {
       </Text>
     {ventilations.map((ventilation, index) => (
       <Box key={index} mb={4} p={4} bg="white" borderRadius="lg" boxShadow="sm">
-          <Flex justify="space-between" align="center">
-            <Text fontWeight="medium">Ventilation {index + 1}</Text>
-            <Tooltip label="Supprimer cette ventilation" hasArrow placement="top">
-              <IconButton
-                aria-label="Remove ventilation"
-                icon={<FcFullTrash />}
-                size="sm"
-                variant="ghost"
-                onClick={() => removeVentilation(index)}
-              />
-            </Tooltip>
-          </Flex>
-          <FormControl mt={4}>
+        <Flex justify="space-between" align="center">
+          <Text fontWeight="medium">Ventilation {index + 1}</Text>
+          <Tooltip label="Supprimer cette ventilation" hasArrow placement="top">
+            <IconButton
+              aria-label="Remove ventilation"
+              icon={<FcFullTrash />}
+              size="sm"
+              variant="ghost"
+              onClick={() => removeVentilation(index)}
+              color={iconColor}
+            />
+          </Tooltip>
+        </Flex>
+        <Stack spacing={4} mt={4}>
+          <FormControl>
             <FormLabel>Catégorie</FormLabel>
             <Select placeholder="Sélectionnez une catégorie" defaultValue={ventilation.category}>
-              {/* Add category options here */}
               <option value="personal">Dépense personnelle</option>
               {/* ... other categories */}
             </Select>
           </FormControl>
-        <FormControl mt={4}>
-          <FormLabel>Montant</FormLabel>
-          <InputGroup>
-            <InputLeftElement
-              pointerEvents="none"
-              children={<MdEuro />} // Use MdEuro icon here
-            />
-            <Input type="number" value={ventilation.amount} />
-          </InputGroup>
-        </FormControl>
-        <FormControl mt={4}>
-          <FormLabel>Pourcentage</FormLabel>
-          <InputGroup>
-            <InputLeftElement
-              pointerEvents="none"
-              children={<FaPercent />} // Use FaPercent icon here
-            />
-            <Input type="number" value={ventilation.percentage} />
-          </InputGroup>
-        </FormControl>
+          <FormControl>
+            <FormLabel>Montant</FormLabel>
+            <InputGroup>
+              <Input type="number" value={ventilation.amount} />
+              <InputRightElement pointerEvents="none" children={<MdEuro color={iconColor} />} />
+            </InputGroup>
+          </FormControl>
+          <FormControl>
+            <FormLabel>Pourcentage</FormLabel>
+            <InputGroup>
+              <Input type="number" value={ventilation.percentage} />
+              <InputRightElement pointerEvents="none" children={<FaPercent color={iconColor} />} />
+            </InputGroup>
+          </FormControl>
+        </Stack>
       </Box>
     ))}
-      <Button leftIcon={<FaPlus />} colorScheme="blue" variant="outline" onClick={addVentilation} mt={2}>
-        Ajouter une ventilation
-      </Button>
-    </Box>
+    <Button leftIcon={<FaPlus />} colorScheme="blue" variant="outline" onClick={addVentilation} mt={2}>
+      Ajouter une ventilation
+    </Button>
+  </Box>
   );
 };
 
