31ca3b533f9138b23aa4b0abac03fcfa4d8f135f
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-04-14 20:14:20 +0000

    ddddddddddd

--- src/components/D2035/D2035.jsx
+++ src/components/D2035/D2035.jsx
@@ -7,35 +7,39 @@ import FinalizeButton from './components/FinalizeButton';
 
 const D2035 = () => {
   const tasks = [
-   'Catégoriser toutes les transactions',
-   'Immobilisations, amortissements et cessions',
-   'Ventiler les repas hors domicile',
-   'Retraiter les frais de véhicule',
-   'Ventiler les échéances d\'emprunts',
-   'Chèques non encaissés de 2022',
-   'Chèques non encaissés de 2023',
-   'Saisir les frais de blanchissage à domicile',
-   'Barème kilométrique forfaitaire',
-   'Vérifier les comptes financiers',
-   'Évolution de la caisse',
-   'Cotisations Urssaf à ventiler',
-   'Saisie du compte commun ou de la SCM',
-   'Réductions d\'impôt pour les frais de comptabilité',
-   'Cotisations facultatives déductibles',
-   'Exonérations fiscales et crédits d\'impôts',
-   'Identité de l\'entrepreneur',
-   'Informations relatives à l\'activité',
-   'Informations relatives à l\'exercice fiscal',
-   'Valider la clôture',
- ];
-  return <Box>
-    <HeaderD2035/>
-    <FinalizeButton/>
-    <Box border={1}>
-    <TaskProgressBar completedTasks={19} totalTasks={20} timeSpent={1} />
-    <TaskList tasks={tasks} />
+    'Catégoriser toutes les transactions',
+    'Immobilisations, amortissements et cessions',
+    'Ventiler les repas hors domicile',
+    'Retraiter les frais de véhicule',
+    'Ventiler les échéances d\'emprunts',
+    'Chèques non encaissés de 2022',
+    'Chèques non encaissés de 2023',
+    'Saisir les frais de blanchissage à domicile',
+    'Barème kilométrique forfaitaire',
+    'Vérifier les comptes financiers',
+    'Évolution de la caisse',
+    'Cotisations Urssaf à ventiler',
+    'Saisie du compte commun ou de la SCM',
+    'Réductions d\'impôt pour les frais de comptabilité',
+    'Cotisations facultatives déductibles',
+    'Exonérations fiscales et crédits d\'impôts',
+    'Identité de l\'entrepreneur',
+    'Informations relatives à l\'activité',
+    'Informations relatives à l\'exercice fiscal',
+    'Valider la clôture',
+  ];
+  return (
+    <Box textAlign="center" alignItems="center" justifyContent="center">
+      <HeaderD2035 />
+      <Box m={4} >
+      <FinalizeButton />
+      </Box>
+      <Box textAlign="center" alignItems="center" justifyContent="center" borderRadius="10px" borderWidth={1} m={4} >
+        <TaskProgressBar completedTasks={19} totalTasks={20} timeSpent={1} />
+        <TaskList tasks={tasks} />
+      </Box>
     </Box>
-   </Box>;
+  );
 };
 
 export default D2035;
