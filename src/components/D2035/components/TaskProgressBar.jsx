920d10b26bcd28d2a4a0525585e04205fd6d762b
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-04-14 19:10:25 +0000

    frdttttttt

--- src/components/D2035/components/TaskProgressBar.jsx
+++ src/components/D2035/components/TaskProgressBar.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import { Box, Text, Flex } from '@chakra-ui/react';
-import { FcAlarmClock } from "react-icons/fc";
+import { FcAlarmClock } from 'react-icons/fc';
 
 const TaskProgressBar = ({ completedTasks, totalTasks, timeSpent }) => {
+  const progressBarHeight = '8px'; // Height for completed tasks
+  const incompleteBarHeight = '2px'; // Height for incomplete tasks
+
   return (
-    <Box width="full" p={4}>
-      {/* Task information and time spent */}
+    <Box width="full" p={4} bg="gray.100" borderRadius="lg">
       <Flex mb={4} justifyContent="space-between" alignItems="center">
-        <Text>Tâches effectuées : {completedTasks} / {totalTasks}</Text>
+        <Text fontWeight="bold">Tâches effectuées : {completedTasks} / {totalTasks}</Text>
         <Flex alignItems="center">
-          <FcAlarmClock />
-          <Text ml={2}>{timeSpent}h</Text>
+          <FcAlarmClock size="1.5em" />
+          <Text ml={2} fontWeight="bold">{timeSpent}h</Text>
         </Flex>
       </Flex>
 
-      {/* Dotted progress bar */}
-      <Flex height="2px" mb={4}>
+      <Flex height={progressBarHeight} mb={4}>
         {Array.from({ length: totalTasks }).map((_, index) => (
           <Box
             key={index}
             flex="1"
-            height="2px"
-            bg={index < completedTasks ? "pink.500" : "gray.300"}
-            mx="2px"
+            height={index < completedTasks ? progressBarHeight : incompleteBarHeight}
+            bg={index < completedTasks ? 'pink.500' : 'gray.200'}
+            mx="1px"
+            borderRadius="md"
           />
         ))}
       </Flex>
