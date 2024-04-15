a1217323071d1197afd6cf383929e762a8ea1e9b
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-04-15 20:53:01 +0000

    dddddddddddd

--- src/components/Profile/components/Tabs/Abonnement/Abonnement.jsx
+++ src/components/Profile/components/Tabs/Abonnement/Abonnement.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Grid, GridItem, VStack } from '@chakra-ui/react';
 import { PaymentMethod } from './components/PaymentMethod';
 import { ParrainezVosProches } from './components/ParrainezVosProches';
 import { ListeDesFactures } from './components/ListeDesFactures';
@@ -7,14 +7,18 @@ import { ResumeAbonnementPremium } from './components/ResumeAbonnementPremium';
 
 export function Abonnement() {
   return (
-    <Grid templateColumns={['1fr', '1fr 1fr']} gap={6}>
-      <GridItem>
-        <PaymentMethod />
-        <ListeDesFactures />
-        <ParrainezVosProches />
+    <Grid templateColumns={['1fr', '1fr 1fr']} gap={6} justifyItems="center">
+      <GridItem w="100%">
+        <VStack spacing={4} align="stretch">
+          <PaymentMethod />
+          <ListeDesFactures />
+          <ParrainezVosProches />
+        </VStack>
       </GridItem>
-      <GridItem>
-        <ResumeAbonnementPremium />
+      <GridItem w="100%">
+        <VStack spacing={4} align="stretch">
+          <ResumeAbonnementPremium />
+        </VStack>
       </GridItem>
     </Grid>
   );
