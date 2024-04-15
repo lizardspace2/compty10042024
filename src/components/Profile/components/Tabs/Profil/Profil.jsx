fd869044c13c7dfb2294891f72d128dad344f6db
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-04-15 21:08:56 +0000

    dddddddddddddcv

--- src/components/Profile/components/Tabs/Profil/Profil.jsx
+++ src/components/Profile/components/Tabs/Profil/Profil.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Box, Heading } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
+import { FormulaireAdresse } from './components/FormulaireAdresse';
+import { InformationsComptablesForm } from './components/InformationsComptablesForm';
+import { InformationsPersonnelles } from './components/InformationsPersonnelles';
 
 export function Profil() {
-  return (
-    <Box p={4} borderWidth="1px" borderRadius="lg">
-      <Heading size="md">Profil</Heading>
-      {/* Add content for Profil here */}
-    </Box>
-  );
+    return (
+        <Box p={4} borderWidth="1px" borderRadius="lg">
+            <InformationsPersonnelles />
+            <FormulaireAdresse />
+            <InformationsComptablesForm />
+        </Box>
+    );
 }
