f627daedf17a77efe7e95d8b7c81e1e896eac15c
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-04-15 22:01:42 +0000

    cwgwdgd

--- src/components/Profile/components/Tabs/Options/Options.jsx
+++ src/components/Profile/components/Tabs/Options/Options.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import { BillingForm } from './components/BillingForm ';
 
 export function Options() {
-    return (
-      <Box>
+  return (
+    <Flex width="100%" justifyContent="center" pt="4" pb="4"> {/* Padding top and bottom for some space */}
+      <Box maxWidth="1000px" width="100%" px="4"> {/* Padding is optional, for some space on the sides */}
         <BillingForm/>
       </Box>
-    );
-  }
+    </Flex>
+  );
+}
