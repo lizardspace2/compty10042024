4ecd05493912173c152febeaa79fd619c9ca4ff8
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-04-15 21:28:30 +0000

    sddfffffsfdsds

--- src/components/Profile/components/Tabs/Declaration/Declarations.jsx
+++ src/components/Profile/components/Tabs/Declaration/Declarations.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Box, Heading } from '@chakra-ui/react';
 import RegimeFiscal from './components/RegimeFiscal';
 import InfoOffreRegimeFiscal from './components/InfoOffreRegimeFiscal';
+
 export function Declarations() {
-    return (
-      <Box>
-        <RegimeFiscal/>
-        <InfoOffreRegimeFiscal/>
+  return (
+    <Box maxWidth="1000px" margin="auto" padding="20px">
+      <RegimeFiscal />
+      <Box mt={8}> {/* Ajoute de l'espace entre les éléments */}
+        <InfoOffreRegimeFiscal />
       </Box>
-    );
-  }
+    </Box>
+  );
+}
