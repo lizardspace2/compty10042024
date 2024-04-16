db7b06f6d7b683caf02fddc329e46a17934b5d9a
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-04-16 19:43:20 +0000

    ddddddddzss

--- src/components/Transactions/components/CategoryComponent.jsx
+++ src/components/Transactions/components/CategoryComponent.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Box, Flex, Heading, Tag, VStack, SimpleGrid, Container, LinkBox, LinkOverlay, useColorModeValue  } from '@chakra-ui/react';
-import { FcBullish, FcDebt, FcFactory, FcAutomotive, FcAlarmClock, FcDonate } from 'react-icons/fc';
+import {
+  Box, Flex, Heading, Tag, VStack, SimpleGrid, Container, LinkBox, LinkOverlay, useColorModeValue
+} from '@chakra-ui/react';
+import {
+  FcBullish, FcDebt, FcFactory, FcAutomotive, FcAlarmClock, FcDonate
+} from 'react-icons/fc';
 
 const categories = {
   Revenues: ['Apport personnel', 'Recette', 'Recette secondaire', 'Redevance de collaboration perçue', 'Autre gain divers', 'Vente d’une immobilisation', 'Emprunt', 'Caution reçue'],
@@ -21,23 +25,15 @@ const icons = {
 };
 
 const CategoryComponent = () => {
-  const hoverBg = useColorModeValue("gray.100", "gray.700"); // Light or dark mode friendly
+  const hoverBg = useColorModeValue("green.100", "green.700") // Light or dark mode friendly hover background
+  const activeBg = useColorModeValue("blue.300", "blue.800"); // Click effect background
 
   return (
     <Container maxW="container.xxl">
       <SimpleGrid columns={6} spacing={5}>
         {Object.keys(categories).map((categoryKey) => (
           <LinkBox as="article" key={categoryKey}>
-            <Box
-              p={5}
-              borderWidth="1px"
-              borderRadius="lg"
-              _hover={{
-                bg: hoverBg,
-                transform: 'scale(1.05)',
-                transition: 'background-color 0.2s, transform 0.2s'
-              }}
-            >
+            <Box p={5} borderWidth="1px" borderRadius="lg">
               <Flex align="center" fontSize="xl">
                 {icons[categoryKey]}
                 <LinkOverlay href={`#${categoryKey.toLowerCase()}`} style={{ marginLeft: 8 }}>
@@ -46,7 +42,15 @@ const CategoryComponent = () => {
               </Flex>
               <VStack align="start">
                 {categories[categoryKey].map((item, index) => (
-                  <Tag size="md" variant="solid" key={index}>
+                  <Tag size="md" variant="solid" key={index} _hover={{
+                    background: hoverBg,
+                    transform: 'scale(1.1)',
+                    transition: 'background-color 0.2s, transform 0.2s'
+                  }} _active={{
+                    background: activeBg,
+                    transform: 'scale(0.9)',
+                    transition: 'background-color 0.1s, transform 0.1s'
+                  }}>
                     {item}
                   </Tag>
                 ))}
