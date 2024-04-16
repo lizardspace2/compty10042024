c83d3525adf3c512f543dad3de1d6f8015628f2e
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-04-16 17:28:35 +0000

    hjjhkjkjlk

--- src/components/Transactions/components/creertransaction/TransactionDetailModal.jsx
+++ src/components/Transactions/components/creertransaction/TransactionDetailModal.jsx
@@ -3,16 +3,71 @@ import {
   Modal,
   ModalOverlay,
   ModalContent,
+  SimpleGrid,
+  Box,
+  Heading,
+  Button,
+  Flex
 } from '@chakra-ui/react';
-import ExpenseTransactionDetail from './ExpenseTransactionDetail'; // Make sure the path is correct
+import ExpenseInformation from './ExpenseInformation';
+import ExpenseVentilationComponent from './ExpenseVentilationComponent';
 
-const TransactionDetailModal = ({ isDetailOpen, onDetailToggle }) => {
+// Define ExpenseFormHeader directly inside TransactionDetailModal
+const ExpenseFormHeader = ({ onToggle }) => {
   return (
-    <Modal isOpen={isDetailOpen} onClose={onDetailToggle} size="full" overflow="auto">
+    <Flex justifyContent="space-between" alignItems="center" p={4} bg="white" boxShadow="md">
+      <Heading as="h3" size="lg">
+        Ajout d'une dépense professionnelle
+      </Heading>
+      <Box>
+        <Button mr={3} onClick={onToggle}>
+          Fermer
+        </Button>
+        <Button colorScheme="pink" onClick={() => { /* Implement add functionality here */ }}>
+          Ajouter
+        </Button>
+      </Box>
+    </Flex>
+  );
+};
+
+// Define ExpenseTransactionDetail directly inside TransactionDetailModal
+const ExpenseTransactionDetail = ({ onToggle }) => {
+  return (
+    <>
+      <ExpenseFormHeader onToggle={onToggle} />
+      <Box
+        p={4}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="flex-start"
+        maxW="1400px"
+        m="0 auto"
+        w="100%"
+      >
+        <SimpleGrid
+          columns={{ base: 1, md: 2 }}
+          spacing={10}
+          width="100%"
+          maxWidth="1400px"
+          margin="0 auto"
+        >
+          <ExpenseInformation />
+          <ExpenseVentilationComponent />
+          {/* Insert additional components if needed */}
+        </SimpleGrid>
+      </Box>
+    </>
+  );
+};
+
+const TransactionDetailModal = ({ isDetailOpen, onToggle }) => {
+  return (
+    <Modal isOpen={isDetailOpen} onClose={onToggle} size="full" overflow="auto">
       <ModalOverlay />
       <ModalContent m={0} maxW="100vw">
-        {/* We include the ExpenseTransactionDetail component here */}
-        <ExpenseTransactionDetail />
+        <ExpenseTransactionDetail onToggle={onToggle} />
       </ModalContent>
     </Modal>
   );
