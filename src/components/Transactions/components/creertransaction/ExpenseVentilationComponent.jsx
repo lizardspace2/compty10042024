d75db3d4b8ba7e2c1a79dc7ca9a780f34c740eeb
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-04-18 17:33:38 +0000

    ezrrrrrrrrrr

--- src/components/Transactions/components/creertransaction/ExpenseVentilationComponent.jsx
+++ src/components/Transactions/components/creertransaction/ExpenseVentilationComponent.jsx
@@ -19,21 +19,45 @@ import {
   ModalContent,
   ModalHeader,
   ModalBody,
+  Container,
+  SimpleGrid,
+  VStack,
+  Tag,
+  Heading,
 } from '@chakra-ui/react';
 import { FaPlus, FaPercent, FaTimes } from 'react-icons/fa';
 import { MdEuro } from 'react-icons/md';
-import { FcFullTrash } from 'react-icons/fc';
-import CategoryComponent from '../CategoryComponent';
-
+import { FcFullTrash, FcBullish, FcDebt, FcFactory, FcAutomotive, FcAlarmClock, FcDonate } from 'react-icons/fc';
 const ExpenseVentilationComponent = () => {
   const [ventilations, setVentilations] = useState([
     { category: 'Dépense personnelle', amount: '-0.99', percentage: 100 },
   ]);
+  const [selectedItem, setSelectedItem] = useState('');
   const [isCategoryModalOpen, setIsCategoryModalOpen] = useState(false); // State to control modal visibility
+
+  const categories = {
+    Revenues: ['Apport personnel', 'Recette', 'Recette secondaire', 'Redevance de collaboration perçue', 'Autre gain divers', 'Vente d’une immobilisation', 'Emprunt', 'Caution reçue'],
+    Remunerations: ['Prélèvement personnel', 'Dépense personnelle', 'Rétrocession versée', 'Redevance de collaboration versée', 'Honoraires payés', '[Salariés] Salaire net', '[Salariés] Impôt à la source', '[Salariés] Charge sociale'],
+    Functionnement: ['Immobilisation', 'Matériel et outillage', 'Achat', 'Frais divers', 'Télécom, fournitures, documents', 'Frais d’acte et de contentieux', 'Débours pour vos clients', 'Virement interne'],
+    Deplacements: ['À catégoriser', 'Formation', 'Réception et congrès', 'Restaurant et repas d’affaires', 'Frais de repas hors domicile', 'Frais de déplacement', 'Véhicule et carburant', 'Location de matériel'],
+    FraisFixes: ['Emprunt', 'Compte commun ou SCM', 'Loyer et charge locative', 'Caution versée', 'Entretien et réparation', 'Abonnement logiciel', 'Eau, gaz, électricité', 'Assurance professionnelle'],
+    CotisationsEtTaxes: ['Cotisation sociale Urssaf', 'Cotisation retraite', 'Cotisation facultative', 'Cotisation professionnelle', 'CFE', 'Autre impôt', 'Amende et pénalité']
+  };
+
+  const icons = {
+    Revenues: <FcBullish />,
+    Remunerations: <FcDebt />,
+    Functionnement: <FcFactory />,
+    Deplacements: <FcAutomotive />,
+    FraisFixes: <FcAlarmClock />,
+    CotisationsEtTaxes: <FcDonate />
+  };
 
   const iconColor = useColorModeValue('gray.200', 'gray.300');
   const bgColor = useColorModeValue('gray.50', 'gray.700');
   const borderColor = useColorModeValue('gray.200', 'gray.600');
+  const hoverBg = useColorModeValue("green.100", "green.700"); // Light or dark mode friendly hover background
+  const activeBg = useColorModeValue("blue.300", "blue.800"); // Click effect background
 
   const addVentilation = () => {
     setVentilations([...ventilations, { category: '', amount: '', percentage: 0 }]);
@@ -117,7 +141,43 @@ const ExpenseVentilationComponent = () => {
             />
           </ModalHeader>
           <ModalBody>
-            <CategoryComponent />
+            <Container maxW="container.xxl">
+              <Input
+                value={selectedItem}
+                placeholder="Click on an item to see it here..."
+                readOnly
+                mb={4}
+              />
+              <SimpleGrid columns={6} spacing={5}>
+                {Object.keys(categories).map((categoryKey) => (
+                  <Box p={5} borderWidth="1px" borderRadius="lg" key={categoryKey}>
+                    <Flex align="center" fontSize="xl">
+                      {icons[categoryKey]}
+                      <Heading as="h3" ml={3} fontSize="xl">{categoryKey}</Heading>
+                    </Flex>
+                    <VStack align="start">
+                      {categories[categoryKey].map((item, index) => (
+                        <Tag size="md" variant="solid" key={index} _hover={{
+                          background: hoverBg,
+                          transform: 'scale(1.1)',
+                          transition: 'background-color 0.2s, transform 0.2s'
+                        }} _active={{
+                          background: activeBg,
+                          transform: 'scale(0.9)',
+                          transition: 'background-color 0.1s, transform 0.1s'
+                        }} onClick={(event) => {
+                          event.preventDefault(); // Prevent default link behavior
+                          setSelectedItem(item);
+                          onCategoryModalClose(); // Close the modal when an item is selected
+                        }}>
+                          {item}
+                        </Tag>
+                      ))}
+                    </VStack>
+                  </Box>
+                ))}
+              </SimpleGrid>
+            </Container>
           </ModalBody>
         </ModalContent>
       </Modal>
