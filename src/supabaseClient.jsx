18312acbaf024325c317a725d269d737180fccf5
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-04-22 17:10:05 +0000

    xxxxxxxxxxxxxxss

---
+++ src/supabaseClient.jsx
@@ -0,0 +1,11 @@
+// supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://iholojdqmdamozagajwk.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlob2xvamRxbWRhbW96YWdhandrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM4MDQ4ODYsImV4cCI6MjAyOTM4MDg4Nn0.LkAy0UWpJiCJ9LS6jnzSYSobzajhgyDAkgoEUq5t-_4';
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Please define all required environment variables!');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
