a7694ed1898e29cdffcd1b91ae753dd6aa1b4cc3
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-04-24 21:35:57 +0000

    ssssssssd

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,69 @@ function App() {
     fetchSession();
   }, []);
 
+  const localization = {
+    variables: {
+      sign_up: {
+        email_label: 'Adresse e-mail',
+        password_label: 'Créer un mot de passe',
+        email_input_placeholder: 'Votre adresse e-mail',
+        password_input_placeholder: 'Votre mot de passe',
+        button_label: 'S\'inscrire',
+        loading_button_label: 'Inscription en cours ...',
+        social_provider_text: 'Se connecter avec {{provider}}',
+        link_text: 'Vous n\'avez pas de compte ? Inscrivez-vous',
+        confirmation_text: 'Vérifiez votre e-mail pour le lien de confirmation',
+      },
+      sign_in: {
+        email_label: 'Adresse e-mail',
+        password_label: 'Votre mot de passe',
+        email_input_placeholder: 'Votre adresse e-mail',
+        password_input_placeholder: 'Votre mot de passe',
+        button_label: 'Se connecter',
+        loading_button_label: 'Connexion en cours ...',
+        social_provider_text: 'Se connecter avec {{provider}}',
+        link_text: 'Vous avez déjà un compte ? Connectez-vous',
+        confirmation_text: 'Vérifiez votre e-mail pour le lien de confirmation',
+      },
+      magic_link: {
+        email_input_label: 'Adresse e-mail',
+        email_input_placeholder: 'Votre adresse e-mail',
+        button_label: 'Se connecter',
+        loading_button_label: 'Connexion en cours ...',
+        link_text: 'Envoyer un e-mail de lien magique',
+        confirmation_text: 'Vérifiez votre e-mail pour le lien magique',
+      },
+      forgotten_password: {
+        email_label: 'Adresse e-mail',
+        password_label: 'Votre mot de passe',
+        email_input_placeholder: 'Votre adresse e-mail',
+        password_input_placeholder: 'Votre mot de passe',
+        button_label: 'Envoyer les instructions de réinitialisation du mot de passe',
+        loading_button_label: 'Envoi des instructions de réinitialisation ...',
+        link_text: 'Mot de passe oublié ?',
+        confirmation_text: 'Vérifiez votre e-mail pour le lien de réinitialisation de mot de passe',
+      },
+      update_password: {
+        password_label: 'Nouveau mot de passe',
+        password_input_placeholder: 'Votre nouveau mot de passe',
+        button_label: 'Mettre à jour le mot de passe',
+        loading_button_label: 'Mise à jour du mot de passe ...',
+        confirmation_text: 'Votre mot de passe a été mis à jour',
+      },
+      verify_otp: {
+        email_input_label: 'Adresse e-mail',
+        email_input_placeholder: 'Votre adresse e-mail',
+        phone_input_label: 'Numéro de téléphone',
+        phone_input_placeholder: 'Votre numéro de téléphone',
+        token_input_label: 'Jeton',
+        token_input_placeholder: 'Votre jeton OTP',
+        button_label: 'Vérifier le jeton',
+        loading_button_label: 'Connexion en cours ...',
+      },
+    },
+  };
+  
+
   return (
     <ChakraProvider>
       <Router>
@@ -40,11 +103,27 @@ function App() {
         ) : (
           <Routes>
             <Route path="/login" element={
-              <Auth
-              supabaseClient={supabase}
-              appearance={{ theme: ThemeSupa }}
-              providers={['google', 'facebook', 'twitter']}  // Ajoutez les fournisseurs que vous souhaitez supporter
-            />
+              <div style={{ 
+                display: 'flex', 
+                alignItems: 'center', 
+                justifyContent: 'center', 
+                height: '100vh', // pour centrer verticalement
+              }}>
+                <Auth
+                  maxW='90%'
+                  textAlign="center"
+                  supabaseClient={supabase}
+                  appearance={{
+                    theme: ThemeSupa, 
+                    style: {
+                      button: { background: 'pink'},
+                      anchor: { color: 'pink' },
+                    },
+                  }}
+                  providers={['google', 'facebook', 'twitter']}  
+                  localization={localization}
+                />
+              </div>
             } />
             <Route path="*" element={<Navigate to="/login" />} />
           </Routes>
